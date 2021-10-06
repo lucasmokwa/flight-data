761c5b88a6e5fc8063efbf250f813198246973fd
Lucas Ravanello Mokwa <lucasravam@gmail.com>
2021-10-06 11:39:56 -0700

    Update to Quarter and Metadata

--- web-client/src/modules/DataInfo.js
+++ web-client/src/modules/DataInfo.js
@@ -3,7 +3,7 @@ const DataInfo = () => {
     <div className='ui raised segment'>
       <p>
         Data extracted from the DB1B Market Survey, provided by the Bureau of
-        Transportation Statistics. Data reflects the 1st Quarter of 2021.{" "}
+        Transportation Statistics. Data reflects the 2nd Quarter of 2021.{" "}
         Project Source Code is available at github.com/lucasmokwa.
       </p>
     </div>
